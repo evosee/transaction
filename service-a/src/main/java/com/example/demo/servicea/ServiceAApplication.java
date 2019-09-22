011d8c0cc0f26a614d9f97c98af10d7630b2a0f2
chensai <chen_sai_@126.com>
2019-09-22 09:13:44 +0800

    commit

--- service-a/src/main/java/com/example/demo/servicea/ServiceAApplication.java
+++ service-a/src/main/java/com/example/demo/servicea/ServiceAApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo.servicea;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.demo.servicea.mapper")
 public class ServiceAApplication {
 
     public static void main(String[] args) {
--- service-a/src/main/java/com/example/demo/servicea/controller/TestController.java
+++ service-a/src/main/java/com/example/demo/servicea/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.demo.servicea.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author chensai
